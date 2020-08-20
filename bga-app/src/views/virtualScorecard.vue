b369c357a3a93df78a8076d871ae2d66134a9c1b
plischke <tplischke@outlook.com>
2020-08-20 14:32:10 -0500

    form now appends form data for multiple players

--- bga-app/src/views/virtualScorecard.vue
+++ bga-app/src/views/virtualScorecard.vue
@@ -28,8 +28,8 @@
             </thead>
             </template>
         </v-simple-table>
-        <!--TODO: create function to push form data to an array to allow inserts for each player per round, then write function to push form data array to backend -->
-        <v-form>
+        <v-form
+        ref="form">
             <v-checkbox
             v-model="isNineRound"
             label="9 Round"
@@ -46,154 +46,182 @@
                 <div v-if="isNineRound">
                     <p>Strokes</p>
                     <v-row>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole1"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole2"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole3"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole4"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole5"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole6"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole7"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole8"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole9"></v-text-field>
-            </v-col>
-                    </v-row>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole1"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole2"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole3"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole4"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole5"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole6"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole7"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole8"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole9"></v-text-field>
+                    </v-col>
+                            </v-row>
                 </div>
                 <div v-if="isEighteenRound">
-                    <p>Strokes</p>
-                    <v-row>
-                        <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole1"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole2"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole3"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole4"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole5"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole6"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole7"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole8"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole9"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole10"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole11"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole12"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole13"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole14"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole15"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole16"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole17"></v-text-field>
-            </v-col>
-            <v-col>
-                <v-text-field
-                label="strokes"
-                v-model="hole18"></v-text-field>
-            </v-col>
-            </v-row>
+                            <p>Strokes</p>
+                            <v-row>
+                                <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole1"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole2"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole3"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole4"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole5"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole6"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole7"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole8"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole9"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole10"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole11"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole12"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole13"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole14"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole15"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole16"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole17"></v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                        outlined
+                        label=""
+                        v-model="hole18"></v-text-field>
+                    </v-col>
+                    </v-row>
                 </div>
             </v-row>
-            <v-btn color="error" @click="clear">Clear form</v-btn>
-            <v-btn color="success">Next Player</v-btn>
+            <v-btn color="error" @click="reset">Clear form</v-btn>
+            <v-btn color="warning" @click="next">Next Player</v-btn>
+            <v-btn color="success" @click="submit"> Submit</v-btn>
         </v-form>
-
-            </v-container>
+        </v-container>
     </v-card>
 </template>
 <script>
@@ -208,6 +236,8 @@ export default {
             courseData: [],
             players: [],
             playerSelected: null,
+            isNineRound: false,
+            isEighteenRound: false,
             hole1:'',
             hole2:'',
             hole3:'',
@@ -226,8 +256,7 @@ export default {
             hole16:'',
             hole17:'',
             hole18:'',
-            isNineRound: false,
-            isEighteenRound: false
+            formdata: new FormData()
         }
     },
     created(){
@@ -262,8 +291,50 @@ export default {
                 console.log(error)
             });
         },
-        clear(){
+        reset(){
             this.$refs.form.reset()
+        },
+        next(){
+            this.formdata.append('course',this.selectedCourse)
+            this.formdata.append('player',this.playerSelected)
+            if(this.isNineRound){
+                this.formdata.append('hole1', this.hole1)
+                this.formdata.append('hole2', this.hole2)
+                this.formdata.append('hole3', this.hole3)
+                this.formdata.append('hole4', this.hole4)
+                this.formdata.append('hole5', this.hole5)
+                this.formdata.append('hole6', this.hole6)
+                this.formdata.append('hole7', this.hole7)
+                this.formdata.append('hole8', this.hole8)
+                this.formdata.append('hole9', this.hole9)
+            }
+            if(this.isEighteenRound){
+                this.formdata.append('hole1', this.hole1)
+                this.formdata.append('hole2', this.hole2)
+                this.formdata.append('hole3', this.hole3)
+                this.formdata.append('hole4', this.hole4)
+                this.formdata.append('hole5', this.hole5)
+                this.formdata.append('hole6', this.hole6)
+                this.formdata.append('hole7', this.hole7)
+                this.formdata.append('hole8', this.hole8)
+                this.formdata.append('hole9', this.hole9)
+                this.formdata.append('hole10', this.hole10)
+                this.formdata.append('hole11', this.hole11)
+                this.formdata.append('hole12', this.hole12)
+                this.formdata.append('hole13', this.hole13)
+                this.formdata.append('hole14', this.hole14)
+                this.formdata.append('hole15', this.hole15)
+                this.formdata.append('hole16', this.hole16)
+                this.formdata.append('hole17', this.hole17)
+                this.formdata.append('hole18', this.hole18)
+            }
+            this.reset()
+            for (var pair of this.formdata.entries()){
+                console.log(pair[0]+','+pair[1])
+            }
+        },
+        submit(){
+            
         }
     },
     computed:{
